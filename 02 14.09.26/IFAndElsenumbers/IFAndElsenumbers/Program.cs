a6171446479348337d67017cd02c23e27a77c785
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-14 11:09:11 +0300

    IFAndElsenumber

---
+++ 02 14.09.26/IFAndElsenumbers/IFAndElsenumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace IFAndElsenumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta enda vanus");
+
+            //peate kasutama if and else lauseid,
+            //et kontrollida, kas kasutaja vanus
+            //on suurem kui 18 või väiksem kui 18
+
+
+
+
+
+            string userInput = Console.ReadLine();
+            int userage = int.Parse(userInput);
+
+            if (userage >= 18)
+            {
+                Console.WriteLine("Sa oled täisealine");
+            }
+            else
+            {
+                Console.WriteLine("Sa oled alaealine");
+            }
+
+        }        
+    }
+}
+
+
+            
