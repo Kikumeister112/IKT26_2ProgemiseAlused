30f8a0f3f4a2bf69b422aa4f0f78afcbf8a74b1d
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-14 10:21:22 +0300

    IFAndElse kleepisin selle sinna faili pealer

---
+++ 02 14.09.26/IFAndElse/IFAndElse/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace IFAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi");
+
+            // siin on muutuja nimega name,
+            // mis on tüübiga string 
+            // loeb andmeid konsoolist ja salvestab
+            // need muutuja name sisse
+            string name = Console.ReadLine();
+            //if ja else kontrollib, kas muutuja
+            //name on tühi või mitte
+
+            //kui muutuja name on tühi, siis väljastab konsoolile
+            //teksti "tere, tudmatu! ERROR" ja teeb 4 piiksu
+            if (name != "")
+            {
+                Console.WriteLine("tere, " + name);
+            }
+
+            else
+            {
+                Console.WriteLine("Tere, tundmatu! ERROR");
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+               
+
+            }
+        }
+    }
+}
+
+
